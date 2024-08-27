2e172f7703fdfb35bd290b6bbde52f94ad18af9f
rezamahesaa <reza.mahesa.azandi@gmail.com>
2024-08-27 09:18:17 +0700

    added New Course feature

---
+++ src/main/java/com/example/demo/service/CourseUserRoleService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.CourseUserRole;
+import com.example.demo.service.generic.GenericService;
+
+public interface CourseUserRoleService extends GenericService<CourseUserRole, Integer> {
+  
+}
