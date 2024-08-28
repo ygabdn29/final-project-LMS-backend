8be3284161b8d1aaa797238ed3cd68c0b9756968
rezamahesaa <reza.mahesa.azandi@gmail.com>
2024-08-28 16:47:46 +0700

    initial commit manage mentee progress rest api

---
+++ src/main/java/com/example/demo/service/ProgressService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Progress;
+import com.example.demo.service.generic.GenericService;
+
+public interface ProgressService extends GenericService<Progress, Integer>{
+  
+}
